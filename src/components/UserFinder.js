75c403c77664cdc914b4d1cf4268d20c06003d9c
Dmitriy Angve <kirirch129@gmail.com>
2022-12-14 09:59:04 +0300

    #

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -8,6 +8,18 @@ const DUMMY_USERS = [
   { id: "u3", name: "Julie" },
 ];
 
+class UserFinder extends Component {
+  constructor() {
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: "",
+    };
+  }
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+}
+
 const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
   const [searchTerm, setSearchTerm] = useState("");
@@ -38,4 +50,13 @@ export default UserFinder;
 // STEP: 1
 // 1.1 Import Component from react
 // 1.2 Define a class named "UserFinder" extends from "Component"
+// 1.3 Need a "constructor()" because I got some state which should be initialized
+// 1.4 Setting this state to an object: "this.state ={}"
+// 1.5 Got two state in "this.state":
+// 1.5.1 "filteredUsers" (come from useState() array) that equal to "DUMMY_USERS"
+// 1.5.2 "searchTerm" (come from another useState() array) that equal  to an empty string
+// 1.6 Next got a function here to search change handler ("searchChangeHandler") which I'll implement as a method in the "UserFinder" class, that should be executed to update the state, at least a part of the state it should update that search term. "searchChangeHandler() {this.setState();}"
+// 1.7 Get to event object because this is bound to the change event on the input and then use the event object to read the value entered by the user ("searchChangeHandler(event) {...") and therefore I, can expect this "event" object in "this.SetState({})" to then set our state always to an object, which is then merged by React and updates the search term part of that state to event target value. ("this.setState({ searchTerm: event.target.value }")
+// 1.8 Only "searchTerm" will be merged, "filteredUsers" won't be lost
+
 // ~ CONVERT USERFINDER COMPONENT TO CLASS-BASED ~
